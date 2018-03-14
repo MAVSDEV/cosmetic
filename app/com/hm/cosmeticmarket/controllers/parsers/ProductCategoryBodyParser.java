34b59e7c9e2eb00f4ca00dda4afe6991f2c05ea9
c5231481 <Uladzimir.Malko@sap.com>
2018-03-14 17:53:00 +0300

    implemented saving of product categories

--- app/com/hm/cosmeticmarket/controllers/parsers/ProductCategoryBodyParser.java
+++ app/com/hm/cosmeticmarket/controllers/parsers/ProductCategoryBodyParser.java
@@ -1,14 +1,14 @@
 package com.hm.cosmeticmarket.controllers.parsers;
 
 import com.google.inject.Inject;
-import com.hm.cosmeticmarket.models.Product;
+import com.hm.cosmeticmarket.models.ProductCategory;
 
 import java.util.concurrent.Executor;
 
 /**
  * Product Category parser from rest api request.
  */
-public class ProductCategoryBodyParser extends AbstractBodyParser<Product> {
+public class ProductCategoryBodyParser extends AbstractBodyParser<ProductCategory> {
     @Inject
     public ProductCategoryBodyParser(Json jsonParser, Executor executor) {
         super(jsonParser, executor);
